81bfedeb4615da75661d7af76e345fe58224a863
vakaheydev <116391267+vakaheydev@users.noreply.github.com>
2025-03-27 18:49:01 +0300

    feature/authorization added authorization controller

--- src/main/java/org/lakas/personalproject/controller/MainController.java
+++ src/main/java/org/lakas/personalproject/controller/MainController.java
@@ -1,44 +1,26 @@
 package org.lakas.personalproject.controller;
 
-import com.theokanning.openai.completion.CompletionRequest;
-import com.theokanning.openai.service.OpenAiService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
-@RequestMapping("/TGAnswer")
+@RequestMapping
 public class MainController {
-
-    @Autowired
-    private OpenAiService serviceAI;
-
-    @GetMapping()
+    @GetMapping
     public String index() {
         return "index";
     }
 
-
-    @GetMapping("/request")
-    public String request(@RequestParam("username") String prompt, Model model) {
-        System.out.printf(prompt);
-
-        String chatModel = "gpt-4o-mini";
-
-        CompletionRequest request = CompletionRequest.builder()
-                .prompt(prompt)
-                .model(chatModel)
-                .maxTokens(100)
-                .temperature(0.7)
-                .build();
-
-        String response = serviceAI.createCompletion(request).getChoices().get(0).getText();
-        System.out.println(response);
-
-        model.addAttribute("response", response);
-        return "answer";
+    @PostMapping("/login")
+    public String getLogin(@RequestParam("login") String login) {
+        log.info("Login: {}", login);
+        // AuthenticationSelenium.startAuthorization(login);
+        return "success";
     }
 }
+
