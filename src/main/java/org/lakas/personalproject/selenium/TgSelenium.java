a79e0a2f23c4c7ee8ce8e8fac5b09f1b8ca58ebf
lakas <VasilyevNA22@st.ithub.ru>
2025-03-27 20:52:53 +0300

    autorization done

---
+++ src/main/java/org/lakas/personalproject/selenium/TgSelenium.java
@@ -0,0 +1,39 @@
+package org.lakas.personalproject.selenium;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class TgSelenium {
+
+    private final WebDriver driver;
+
+    private WebElement enterLine;
+
+    @Autowired
+    public TgSelenium(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void readMessages() {
+        waitForAuthorization();
+        log.info("Found element");
+        enterLine.click();
+        enterLine.sendKeys("TEST", Keys.ENTER);
+    }
+
+    private void waitForAuthorization() {
+        By findParameter = By.className("input-message-input");
+
+        log.info("Waiting for authorization...");
+        while (enterLine == null) {
+            try {
+                enterLine = driver.findElement(findParameter);
+            } catch (NoSuchElementException e) {}
+        }
+    }
+}
